5decdbbeb58c639487cc775a29751228b0a3e1ac
bonma7 <arnaudbonma@hotmail.com>
2021-02-08 23:04:14 +0000

    add hero + data

---
+++ pages/components/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// 1. Import `InlineTextarea`
+import { InlineTextarea } from 'react-tinacms-inline'
+
+export function Hero() {
+
+  // 2. Replace `data` with Inline Fields
+  return (
+    <div className="hero">
+      <div className="wrapper wrapper--narrow">
+       <h1>
+        <InlineTextarea name="headline" />
+       </h1>
+       <p>
+         <InlineTextarea name="subtext" />
+       </p>
+      </div>
+    </div>
+  )
+}
